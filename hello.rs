c750e0b2a3691aea29a393f5029a9552ef397161
ardianpramana <paramana.ardian@gmail.com>
2024-12-20 17:03:15 +0700

    [ADD] hello.rs: just Hello World and comments

---
+++ hello.rs
@@ -0,0 +1,34 @@
+// This is a comment, and is ignored by the compiler.
+// You can test this code by clicking the "Run" button over there ->
+// or if you prefer to use your keyboard, you can use the "Ctrl + Enter"
+// shortcut.
+
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // 1. Hello World
+    // Print text to the console.
+    println!("1. Hello World");
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+    println!("");
+
+    /*
+    1.1. Comments
+    Above is the first step to run rust
+    */
+    let x = 5 + 90 + 5;
+    let y = 5 + /* 90 + */ 5;
+    println!("1.1. Comments");
+    println!("Is 'x' 10 or 100? x = {}", x);
+    println!("Is 'y' 10 or 100? x = {}", y);
+    println!("");
+
+    /*
+
+    */
+}
